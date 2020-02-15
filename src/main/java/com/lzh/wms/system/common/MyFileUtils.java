bf79b338c20f42938480f4c80b591a24c112cf05
liuyang <15112158974@139.com>
2020-02-15 23:56:50 +0800

    商品管理 image upload

---
+++ src/main/java/com/lzh/wms/system/common/MyFileUtils.java
@@ -0,0 +1,79 @@
+package com.lzh.wms.system.common;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 文件上传下载工具类
+ *
+ * @author lzh
+ * @date 2020-02-15 22:19
+ */
+public class MyFileUtils {
+
+    /**
+     * 文件上传的保存路径
+     */
+    public static String UPLOAD_PATH="E:/upload/";
+
+    /**
+     * 读取配置文件的存储地址
+     */
+    static {
+        InputStream stream = MyFileUtils.class.getClassLoader().getResourceAsStream("file.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String property = properties.getProperty("filepath");
+        if (property != null) {
+            UPLOAD_PATH = property;
+        }
+    }
+
+    /**
+     * 根据原文件名生成新的文件名
+     * @param originalFileName
+     * @return
+     */
+    public static String createFileName(String originalFileName) {
+        String suffix = originalFileName.substring(originalFileName.lastIndexOf("."), originalFileName.length());
+        return IdUtil.simpleUUID().toUpperCase()+suffix;
+    }
+
+    /**
+     * 文件下载
+     * @param path
+     * @return
+     */
+    public static ResponseEntity<Object> createResponseEntity(String path) {
+        //1.构造文件对象
+        File file = new File(UPLOAD_PATH,path);
+        if (file.exists()) {
+            //将下载的文件封装成byte[]数组
+            byte[] bytes=null;
+            bytes = FileUtil.readBytes(file);
+            //创建封装响应头信息的对象
+            HttpHeaders headers = new HttpHeaders();
+            //封装响应内容类型(APPLICATION_OCTET_STREAM 响应的内容不限定)
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            //设置下载的文件的名称
+            //headers.setContentDispositionFormData("attachment","123.jpg");
+            //创建ResponseEntity对象
+            ResponseEntity<Object> entity = new ResponseEntity<>(bytes,headers, HttpStatus.CREATED);
+            return entity;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/lzh/wms/system/controller/FileController.java
@@ -0,0 +1,70 @@
+package com.lzh.wms.system.controller;
+
+import cn.hutool.core.date.DateUtil;
+import com.lzh.wms.system.common.MyFileUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 文件上传下载
+ *
+ * @author lzh
+ * @date 2020-02-15 22:16
+ */
+@RestController
+@RequestMapping("file")
+public class FileController {
+
+    /**
+     * 文件上传
+     * @param mf
+     * @return
+     */
+    @RequestMapping("uploadFile")
+    public Map<String,Object> uploadFile(MultipartFile mf){
+        //1.得到文件名
+        String originalFileName = mf.getOriginalFilename();
+        //2.根据文件名生成新的文件名
+        String newName = MyFileUtils.createFileName(originalFileName);
+        //3.得到当前日期的字符串
+        String dirName = DateUtil.format(new Date(), "yyyy-MM-dd");
+        //4.构造文件夹
+        File dirFile = new File(MyFileUtils.UPLOAD_PATH,dirName);
+        //5.判断当前文件夹是否存在
+        if (!dirFile.exists()){
+            //创建文件夹
+            dirFile.mkdirs();
+        }
+        //6.构造文件对象
+        File file = new File(dirFile,newName);
+        //7.将mf里的图片信息写入file
+        try {
+            mf.transferTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<String,Object> map = new HashMap<>();
+        //返回相对的路径
+        map.put("path",dirName+"/"+newName);
+        return map;
+    }
+
+    /**
+     * 图片下载
+     * @param path
+     * @return
+     */
+    @RequestMapping("showImageByPath")
+    public ResponseEntity<Object> showImageByPath(String path){
+        return MyFileUtils.createResponseEntity(path);
+    }
+
+}
